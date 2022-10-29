7630e7d1bf55ed89efc0e7f74d22b28f9a33fc32
Aditya <75035219+pegvin@users.noreply.github.com>
2022-10-29 18:23:55 +0530

    Get Terminal Size

--- src/main.c
+++ src/main.c
@@ -1,3 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+#include <pty.h>
+#include <termios.h>
+
+// Uses Simple Escape Codes To Get The Cursor Positon
+int TermGetCursorPos(int *rows, int *cols) {
+	char buf[32];
+	unsigned int i = 0;
+
+	if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
+
+	while (i < sizeof(buf) - 1) {
+		if (read(STDIN_FILENO, &buf[i], 1) != 1) break;
+		if (buf[i] == 'R') break;
+		i++;
+	}
+	buf[i] = '\0';
+
+	if (buf[0] != '\x1b' || buf[1] != '[') return -1;
+	if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
+
+	return 0;
+}
+
+// Uses Simple Escape Codes To Get The Window Size in Rows & Columns & Not Pixels
+int TermGetWinSize(int *rows, int *cols) {
+	struct winsize ws;
+
+	if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+		if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
+		return TermGetCursorPos(rows, cols);
+	} else {
+		*cols = ws.ws_col;
+		*rows = ws.ws_row;
+		return 0;
+	}
+}
+
 int main(int argc, char** argv) {
+	int rows = 0; // Width
+	int cols = 0; // Height
+	TermGetWinSize(&rows, &cols);
+	printf("Terminal Size: %dx%d\n", cols, rows);
 	return 0;
 }
